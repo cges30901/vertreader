9667ada71156763ef67664e24cada9be631a8b9f
Hsiu-Ming Chang <cges30901@gmail.com>
2020-02-11 22:48:51 +0800

    Scroll horizontally with mouse wheel

--- vertreader/mainwindow.py
+++ vertreader/mainwindow.py
@@ -4,9 +4,7 @@ import ebooklib
 from ebooklib import epub
 import tempfile
 import zipfile
-from PyQt5.QtWebEngineWidgets import QWebEngineView
-from PyQt5.QtCore import QUrl
-from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtCore import QUrl, QEvent, pyqtSlot
 from vertreader.ui_mainwindow import Ui_MainWindow
 
 
@@ -20,6 +18,19 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.book = None
         self.doc = []
         self.docIndex = 0
+        self.view.focusProxy().installEventFilter(self)
+
+    def eventFilter(self, source, e):
+        if source == self.view.focusProxy() and e.type() == QEvent.Wheel:
+            # The coordinate of javascript and Qt WebEngine is different.
+            # In javascript, the beginning of document is 0.
+            # In Qt WebEngine, the end of document is 0.
+            # So I have to convert it to use javascript to scroll.
+            pos_js = self.view.page().scrollPosition().x() - self.view.page().contentsSize().width() + self.view.width()
+            self.view.page().runJavaScript("window.scrollTo({0}, {1});"
+                .format(pos_js + e.angleDelta().y(), self.view.page().scrollPosition().y()))
+            return True
+        return False
 
     @pyqtSlot()
     def on_action_Open_triggered(self):
@@ -43,7 +54,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
     @pyqtSlot(bool)
     def on_btnPrev_clicked(self):
-        self.docIndex-=1
+        self.docIndex -= 1
         self.btnNext.setEnabled(True)
         if self.docIndex == 1:
             self.btnPrev.setEnabled(False)
@@ -51,7 +62,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
     @pyqtSlot(bool)
     def on_btnNext_clicked(self):
-        self.docIndex+=1
+        self.docIndex += 1
         self.btnPrev.setEnabled(True)
         if self.docIndex == len(self.doc) - 1:
             self.btnNext.setEnabled(False)
