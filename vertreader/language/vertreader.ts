107ee74ec7bcadb040ed8e66390143d53df6b9af
Hsiu-Ming Chang <cges30901@gmail.com>
2020-02-23 07:16:00 +0800

    Run pylupdate5 and update zh_TW translation

--- vertreader/language/vertreader.ts
+++ vertreader/language/vertreader.ts
@@ -23,12 +23,12 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="106"/>
+        <location filename="../mainwindow.ui" line="114"/>
         <source>&amp;Open</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.py" line="50"/>
+        <location filename="../mainwindow.py" line="77"/>
         <source>EPUB documents (*.epub)</source>
         <translation type="unfinished"></translation>
     </message>
@@ -48,17 +48,17 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="111"/>
+        <location filename="../mainwindow.ui" line="119"/>
         <source>&amp;About</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.py" line="137"/>
+        <location filename="../mainwindow.py" line="171"/>
         <source>About</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.py" line="137"/>
+        <location filename="../mainwindow.py" line="171"/>
         <source>&lt;h3&gt;VertReader {0}&lt;/h3&gt;
 &lt;p&gt;Author: Hsiu-Ming Chang&lt;/p&gt;
 &lt;p&gt;e-mail: cges30901@gmail.com&lt;/p&gt;
@@ -66,13 +66,28 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.py" line="145"/>
+        <location filename="../mainwindow.py" line="179"/>
         <source>Libraries used</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.py" line="145"/>
+        <location filename="../mainwindow.py" line="179"/>
         <source>&lt;h3&gt;Libraries:&lt;/h3&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="101"/>
+        <source>&amp;View</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="135"/>
+        <source>&amp;Paged Mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="143"/>
+        <source>&amp;Continuous Mode</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
--- vertreader/language/vertreader_zh_TW.ts
+++ vertreader/language/vertreader_zh_TW.ts
@@ -24,12 +24,12 @@
         <translation>目錄(&amp;T)</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="106"/>
+        <location filename="../mainwindow.ui" line="114"/>
         <source>&amp;Open</source>
         <translation>開啟(&amp;O)</translation>
     </message>
     <message>
-        <location filename="../mainwindow.py" line="50"/>
+        <location filename="../mainwindow.py" line="77"/>
         <source>EPUB documents (*.epub)</source>
         <translation>EPUB 文件 (*.epub)</translation>
     </message>
@@ -49,17 +49,17 @@
         <translation>說明(&amp;H)</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="111"/>
+        <location filename="../mainwindow.ui" line="119"/>
         <source>&amp;About</source>
         <translation>關於(&amp;A)</translation>
     </message>
     <message>
-        <location filename="../mainwindow.py" line="137"/>
+        <location filename="../mainwindow.py" line="171"/>
         <source>About</source>
         <translation>關於</translation>
     </message>
     <message>
-        <location filename="../mainwindow.py" line="137"/>
+        <location filename="../mainwindow.py" line="171"/>
         <source>&lt;h3&gt;VertReader {0}&lt;/h3&gt;
 &lt;p&gt;Author: Hsiu-Ming Chang&lt;/p&gt;
 &lt;p&gt;e-mail: cges30901@gmail.com&lt;/p&gt;
@@ -70,14 +70,29 @@
 &lt;p&gt;授權：GPL v3&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="../mainwindow.py" line="145"/>
+        <location filename="../mainwindow.py" line="179"/>
         <source>Libraries used</source>
         <translation>使用的函式庫</translation>
     </message>
     <message>
-        <location filename="../mainwindow.py" line="145"/>
+        <location filename="../mainwindow.py" line="179"/>
         <source>&lt;h3&gt;Libraries:&lt;/h3&gt;</source>
         <translation>&lt;h3&gt;函式庫：&lt;/h3&gt;</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="101"/>
+        <source>&amp;View</source>
+        <translation>檢視(&amp;V)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="135"/>
+        <source>&amp;Paged Mode</source>
+        <translation>頁面模式(&amp;P)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="143"/>
+        <source>&amp;Continuous Mode</source>
+        <translation>連續模式(&amp;C)</translation>
     </message>
 </context>
 </TS>
