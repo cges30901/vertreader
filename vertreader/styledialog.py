bcf36f33eb3ebb948519169912e21e552346b59c
Hsiu-Ming Chang <cges30901@gmail.com>
2020-02-24 22:18:07 +0800

    Add button to set color

--- vertreader/styledialog.py
+++ vertreader/styledialog.py
@@ -1,8 +1,14 @@
 from vertreader.ui_styledialog import Ui_StyleDialog
-from PyQt5.QtWidgets import QDialog
+from PyQt5.QtWidgets import QDialog, QColorDialog
+from PyQt5.QtCore import pyqtSlot
 
 class StyleDialog(QDialog, Ui_StyleDialog):
 
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
+
+    @pyqtSlot()
+    def on_btnColor_clicked(self):
+        color = QColorDialog.getColor()
+        self.btnColor.setStyleSheet("border: none; background-color: " + color.name())
