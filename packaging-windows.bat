ea79acec6d602e07627388c0a4210b793a9e0078
Hsiu-Ming Chang <cges30901@gmail.com>
2020-03-03 23:33:54 +0800

    Package translation on Windows

--- packaging-windows.bat
+++ packaging-windows.bat
@@ -1,6 +1,7 @@
 pyinstaller -w -y -n vertreader ^
 -i vertreader.ico ^
 --add-data="vertreader\vertreader.svg;vertreader" ^
+--add-data="vertreader\language\*;language" ^
 --add-data="LICENSE.txt;." ^
 vertreader\__init__.py
 
