4039be0833e06c7958d2d246de894da59b59a384
Hsiu-Ming Chang <cges30901@gmail.com>
2020-03-04 20:58:00 +0800

    Package ebooklib license on Windows

--- packaging-windows.bat
+++ packaging-windows.bat
@@ -3,6 +3,7 @@ pyinstaller -w -y -n vertreader ^
 --add-data="vertreader\vertreader.svg;vertreader" ^
 --add-data="vertreader\language\*;language" ^
 --add-data="LICENSE.txt;." ^
+--add-data="vertreader\ebooklib\LICENSE.txt;ebooklib" ^
 vertreader\__init__.py
 
 pause
