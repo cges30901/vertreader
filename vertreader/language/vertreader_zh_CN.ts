03a6b79fb17ca49ef7bd6ebdd1d99c261a1622a3
Hsiu-Ming Chang <cges30901@gmail.com>
2020-04-11 16:13:16 +0800

    Add zh_CN translation

---
+++ vertreader/language/vertreader_zh_CN.ts
@@ -0,0 +1,195 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.ui" line="41"/>
+        <source>&amp;Next</source>
+        <translation>下一个(&amp;N)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="51"/>
+        <source>&amp;Previous</source>
+        <translation>上一个(&amp;P)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="83"/>
+        <source>&amp;File</source>
+        <translation>文件(&amp;F)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="92"/>
+        <source>&amp;TOC</source>
+        <translation>目录(&amp;T)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="119"/>
+        <source>&amp;Open</source>
+        <translation>打开(&amp;O)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="96"/>
+        <source>EPUB documents (*.epub)</source>
+        <translation>EPUB 文档 (*.epub)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="28"/>
+        <source>about:blank</source>
+        <translation>about:blank</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="14"/>
+        <source>VertReader</source>
+        <translation>VertReader</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="97"/>
+        <source>&amp;Help</source>
+        <translation>帮助(&amp;H)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="124"/>
+        <source>&amp;About</source>
+        <translation>关于(&amp;A)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="192"/>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="192"/>
+        <source>&lt;h3&gt;VertReader {0}&lt;/h3&gt;
+&lt;p&gt;Author: Hsiu-Ming Chang&lt;/p&gt;
+&lt;p&gt;e-mail: cges30901@gmail.com&lt;/p&gt;
+&lt;p&gt;License: GPL v3&lt;/p&gt;</source>
+        <translation>&lt;h3&gt;VertReader {0}&lt;/h3&gt;
+&lt;p&gt;作者：张修铭&lt;/p&gt;
+&lt;p&gt;电子邮件：cges30901@gmail.com&lt;/p&gt;
+&lt;p&gt;授权：GPL v3&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="222"/>
+        <source>Libraries used</source>
+        <translation>使用的库</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="222"/>
+        <source>&lt;h3&gt;Libraries:&lt;/h3&gt;</source>
+        <translation>&lt;h3&gt;库：&lt;/h3&gt;</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="104"/>
+        <source>&amp;View</source>
+        <translation>视图(&amp;V)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="140"/>
+        <source>&amp;Paged Mode</source>
+        <translation>页面模式(&amp;P)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="148"/>
+        <source>&amp;Continuous Mode</source>
+        <translation>连续模式(&amp;C)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="153"/>
+        <source>&amp;Style</source>
+        <translation>样式(&amp;S)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="158"/>
+        <source>&amp;Metadata</source>
+        <translation>元数据(&amp;M)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="112"/>
+        <source>Failed to extract</source>
+        <translation>解压失败</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="122"/>
+        <source>Failed to read EPUB with EbookLib</source>
+        <translation>使用 EbookLib 读取 EPUB 失败</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="144"/>
+        <source>Failed to read TOC</source>
+        <translation>读取目录失败</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="158"/>
+        <source>{} - VertReader</source>
+        <translation>{} - VertReader</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="212"/>
+        <source>N/A</source>
+        <translation>无</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="213"/>
+        <source>Metadata</source>
+        <translation>元数据</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="213"/>
+        <source>Title: {0}
+Author: {1}
+Description: {2}</source>
+        <translation>书名：{0}
+作者：{1}
+描述：{2}</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="218"/>
+        <source>Failed to read metadata</source>
+        <translation>无法读取元数据</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="168"/>
+        <source>&amp;Search</source>
+        <translation>搜索(&amp;S)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="144"/>
+        <source>Failed to read TOC: link to unrecognized item is &quot;{}&quot;</source>
+        <translation>读取目录失败：无法辨识的连结是「{}」</translation>
+    </message>
+</context>
+<context>
+    <name>StyleDialog</name>
+    <message>
+        <location filename="../styledialog.ui" line="14"/>
+        <source>Style</source>
+        <translation>样式</translation>
+    </message>
+    <message>
+        <location filename="../styledialog.ui" line="22"/>
+        <source>Zoom Factor:</source>
+        <translation>缩放系数：</translation>
+    </message>
+    <message>
+        <location filename="../styledialog.ui" line="62"/>
+        <source>Text color:</source>
+        <translation>文字颜色：</translation>
+    </message>
+    <message>
+        <location filename="../styledialog.ui" line="96"/>
+        <source>Background color:</source>
+        <translation>背景颜色：</translation>
+    </message>
+    <message>
+        <location filename="../styledialog.ui" line="128"/>
+        <source>Vertical writing</source>
+        <translation>纵书</translation>
+    </message>
+    <message>
+        <location filename="../styledialog.ui" line="138"/>
+        <source>Horizontal writing</source>
+        <translation>横书</translation>
+    </message>
+</context>
+</TS>
--- vertreader/vertreader.pro
+++ vertreader/vertreader.pro
@@ -4,4 +4,5 @@ FORMS += mainwindow.ui \
     styledialog.ui
 
 TRANSLATIONS=language/vertreader.ts \
+    language/vertreader_zh_CN.ts \
     language/vertreader_zh_TW.ts
