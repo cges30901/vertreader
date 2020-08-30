b8021ed6a5ef128d8dedf74a359a406f799fc8f9
Hsiu-Ming Chang <cges30901@gmail.com>
2020-08-30 09:33:48 +0800

    Display popup footnote with a custom modal box
    Style in popup footnote is now supported

--- vertreader/footnote.js
+++ vertreader/footnote.js
@@ -1,14 +1,42 @@
+var modal = document.createElement("div");
+modal.style.display = "none";
+modal.style.position = "fixed";
+modal.style.zIndex = 1;
+modal.style.left = 0;
+modal.style.top = 0;
+modal.style.width = "100%";
+modal.style.height = "100%";
+modal.style.backgroundColor = "rgba(0,0,0,0.4)";
+modal.style.flexDirection = "column-reverse";// Put modalContent to the left
+
+var modalContent = document.createElement("div");
+modalContent.style.backgroundColor = document.body.style.backgroundColor;
+modalContent.style.maxWidth = "50%";
+modalContent.style.overflow = "auto";
+modalContent.style.height = "100%";
+modalContent.style.padding = "10px";
+modalContent.style.border = "1px solid";
+
+modal.appendChild(modalContent);
+document.body.appendChild(modal);
+
 var el = document.querySelectorAll('a');
 for(var i = 0; i < el.length; i++){
     if(el[i].getAttribute("epub:type") == "noteref"){
-        note = document.getElementById(el[i].getAttribute("href").slice(1));
         el[i].addEventListener("click", function(event){
-            showFootnote(note.innerText);
             event.preventDefault();
+            // Get href of footnote
+            var href = event.currentTarget.getAttribute("href");
+            // Remove characters before and including # to get id
+            var note = document.getElementById(href.slice(href.lastIndexOf("#") + 1));
+            modalContent.innerHTML = note.innerHTML;
+            modal.style.display = "flex";
         });
     }
 }
 
-function showFootnote(note) {
-  window.alert(note);
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
