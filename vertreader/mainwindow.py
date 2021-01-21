73c3f0bdcfafbeb2f53cb5537847cdb9cdd9cdb3
Hsiu-Ming Chang <cges30901@gmail.com>
2021-01-21 23:47:29 +0800

    Remove continuous and horizontal mode

--- vertreader/mainwindow.py
+++ vertreader/mainwindow.py
@@ -18,9 +18,6 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         super().__init__()
         self.setWindowIcon(QIcon(os.path.dirname(os.path.abspath(__file__))+'/vertreader.svg'))
         self.setupUi(self)
-        group = QActionGroup(self)
-        group.addAction(self.actionPaged)
-        group.addAction(self.actionScroll)
 
         self.filename = args.file
         self.tempdir = ''
@@ -36,7 +33,6 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         self.color = "black"
         self.bgColor = "white"
-        self.isVertical = True
         self.readSettings()
 
         self.view.focusProxy().installEventFilter(self)
@@ -52,43 +48,19 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                 # Reload when resized to paginate again
                 self.view.reload()
                 self.pageIndex = 0
-            if self.actionPaged.isChecked():
-                if e.type() == QEvent.Wheel:
-                    if e.angleDelta().y() > 0:
-                        self.gotoPage(-1)
-                    elif e.angleDelta().y() < 0:
-                        self.gotoPage(1)
+            elif e.type() == QEvent.Wheel:
+                if e.angleDelta().y() > 0:
+                    self.gotoPage(-1)
+                elif e.angleDelta().y() < 0:
+                    self.gotoPage(1)
+                return True
+            elif e.type() == QEvent.KeyPress:
+                if e.key() == Qt.Key_PageDown or e.key() == Qt.Key_Down:
+                    self.gotoPage(1)
                     return True
-                elif e.type() == QEvent.KeyPress:
-                    if e.key() == Qt.Key_PageDown or e.key() == Qt.Key_Down:
-                        self.gotoPage(1)
-                        return True
-                    elif e.key() == Qt.Key_PageUp or e.key() == Qt.Key_Up:
-                        self.gotoPage(-1)
-                        return True
-            elif self.actionScroll.isChecked() and self.isVertical:
-                # The coordinate of javascript and Qt WebEngine is different.
-                # In javascript, the beginning of document is 0.
-                # In Qt WebEngine, the end of document is 0.
-                # So I have to convert it to use javascript to scroll.
-                pos_js = self.view.page().scrollPosition().x() - self.view.page().contentsSize().width() + self.view.width()
-                # Adjust position according to zoomFactor
-                pos_js /= self.view.zoomFactor()
-
-                if e.type() == QEvent.Wheel:
-                    self.view.page().runJavaScript("window.scrollTo({0}, {1});"
-                        .format(pos_js + e.angleDelta().y(), self.view.page().scrollPosition().y()))
+                elif e.key() == Qt.Key_PageUp or e.key() == Qt.Key_Up:
+                    self.gotoPage(-1)
                     return True
-                elif e.type() == QEvent.KeyPress and e.key() == Qt.Key_PageUp:
-                    self.view.page().runJavaScript("window.scrollTo({0}, {1});"
-                        .format(pos_js + self.view.width() * 0.9, self.view.page().scrollPosition().y()))
-                    return True
-                elif e.type() == QEvent.KeyPress and e.key() == Qt.Key_PageDown:
-                    self.view.page().runJavaScript("window.scrollTo({0}, {1});"
-                        .format(pos_js - self.view.width() * 0.9, self.view.page().scrollPosition().y()))
-                    return True
-            else: # No need to scroll manually in continuous mode and horizontal writing
-                return False
         return False
 
     @pyqtSlot()
@@ -163,7 +135,6 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         self.readSettings()
         self.need_scroll = True
-        self.setButtons()
 
         self.view.load(QUrl.fromLocalFile(self.doc[self.docIndex]))
         self.setWindowTitle(self.tr("{} - VertReader").format(self.book.get_metadata('DC', 'title')[0][0]))
@@ -180,22 +151,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                 self.view.load(url)
 
                 self.docIndex = self.toc[x][3]
-                self.setButtons()
                 break
-
-    @pyqtSlot(bool)
-    def on_btnPrev_clicked(self):
-        self.docIndex -= 1
-        self.pageIndex = 0
-        self.setButtons()
-        self.view.load(QUrl.fromLocalFile(self.doc[self.docIndex]))
-
-    @pyqtSlot(bool)
-    def on_btnNext_clicked(self):
-        self.docIndex += 1
-        self.pageIndex = 0
-        self.setButtons()
-        self.view.load(QUrl.fromLocalFile(self.doc[self.docIndex]))
 
     @pyqtSlot()
     def on_action_About_triggered(self):
@@ -243,11 +199,9 @@ Description: {2}''').format(title, author, description))
             settings.setValue("isMaximized", self.isMaximized())
             settings.setValue("width", self.width())
             settings.setValue("height", self.height())
-            settings.setValue("ispagedview", self.actionPaged.isChecked())
             settings.setValue("zoomFactor", self.view.zoomFactor())
             settings.setValue("color", self.color)
             settings.setValue("bgColor", self.bgColor)
-            settings.setValue("isVertical", self.isVertical)
             settings.setValue("docIndex", self.docIndex)
             settings.setValue("pageIndex", self.pageIndex)
             settings.setValue("posX", self.view.page().scrollPosition().x()
@@ -273,17 +227,10 @@ Description: {2}''').format(title, author, description))
         else:
             self.showNormal()
             self.resize(int(settings.value("width", self.width())), int(settings.value("height", self.height())))
-        if settings.value("ispagedview", self.actionPaged.isChecked(), type = bool) == True:
-            self.actionPaged.setChecked(True)
-            self.actionScroll.setChecked(False)
-        else:
-            self.actionPaged.setChecked(False)
-            self.actionScroll.setChecked(True)
 
         self.view.setZoomFactor(float(settings.value("zoomFactor", 0)))
         self.color = settings.value("color", self.color)
         self.bgColor = settings.value("bgColor", self.bgColor)
-        self.isVertical = settings.value("isVertical", self.isVertical, type = bool)
         self.docIndex = int(settings.value("docIndex", 0))
         self.pageIndex = int(settings.value("pageIndex", 0))
         self.posX = int(settings.value("posX", 0))
@@ -296,7 +243,6 @@ Description: {2}''').format(title, author, description))
         for index, item in enumerate(self.doc):
             if self.view.url().toLocalFile() == item:
                 self.docIndex = index
-                self.setButtons()
 
         self.view.page().runJavaScript('document.body.style.color="{}"'.format(self.color))
         self.view.page().runJavaScript('document.body.style.backgroundColor="{}"'.format(self.bgColor))
@@ -306,33 +252,21 @@ Description: {2}''').format(title, author, description))
             js = jsfile.read()
         self.view.page().runJavaScript(js)
 
-        if self.isVertical:
-            self.view.page().runJavaScript('document.body.style.writingMode="vertical-rl"')
-        else:
-            self.view.page().runJavaScript('document.body.style.writingMode="horizontal-tb"')
+        self.view.page().runJavaScript('document.body.style.writingMode="vertical-rl"')
 
-        if self.actionPaged.isChecked():
-            def paginateFinished(callback):
-                self.pageCount = callback[0]
-                if callback[1] == 1:
-                    #pagination failed
-                    QMessageBox.warning(self, self.tr("Pagination failed"),
-                        self.tr('Pagination failed. Please report to developer.'))
-                if self.pageIndex == -1:
-                    if self.isVertical:
-                        self.view.page().runJavaScript("window.scrollTo(0,document.body.scrollHeight);")
-                    else:
-                        self.view.page().runJavaScript("window.scrollTo(document.body.scrollWidth,0);")
-                    self.pageIndex = self.pageCount - 1
+        def paginateFinished(callback):
+            self.pageCount = callback[0]
+            if callback[1] == 1:
+                #pagination failed
+                QMessageBox.warning(self, self.tr("Pagination failed"),
+                    self.tr('Pagination failed. Please report to developer.'))
+            if self.pageIndex == -1:
+                self.view.page().runJavaScript("window.scrollTo(0,document.body.scrollHeight);")
+                self.pageIndex = self.pageCount - 1
 
-            if self.isVertical:
-                with open(os.path.dirname(os.path.abspath(__file__))+'/paginate_vertical.js', 'r') as jsfile:
-                    js = jsfile.read()
-                self.view.page().runJavaScript(js, paginateFinished)
-            else:
-                with open(os.path.dirname(os.path.abspath(__file__))+'/paginate_horizontal.js', 'r') as jsfile:
-                    js = jsfile.read()
-                self.view.page().runJavaScript(js, paginateFinished)
+        with open(os.path.dirname(os.path.abspath(__file__))+'/paginate_vertical.js', 'r') as jsfile:
+            js = jsfile.read()
+        self.view.page().runJavaScript(js, paginateFinished)
 
         if self.need_scroll is True:
             self.need_scroll = False
@@ -342,24 +276,6 @@ Description: {2}''').format(title, author, description))
         if self.isSearching:
             self.search()
 
-    def setButtons(self):
-        if self.docIndex == 0:
-            self.btnPrev.setEnabled(False)
-        else:
-            self.btnPrev.setEnabled(True)
-        if self.docIndex == len(self.doc) - 1:
-            self.btnNext.setEnabled(False)
-        else:
-            self.btnNext.setEnabled(True)
-
-    @pyqtSlot()
-    def on_actionPaged_triggered(self):
-        self.view.reload()
-
-    @pyqtSlot()
-    def on_actionScroll_triggered(self):
-        self.view.reload()
-
     @pyqtSlot()
     def on_action_Style_triggered(self):
         dlgStyle=StyleDialog(self)
@@ -368,28 +284,19 @@ Description: {2}''').format(title, author, description))
         dlgStyle.btnColor.setStyleSheet("border: none; background-color: " + self.color)
         dlgStyle.bgColor = self.bgColor
         dlgStyle.btnBgColor.setStyleSheet("border: none; background-color: " + self.bgColor)
-        dlgStyle.btnVertical.setChecked(self.isVertical)
-        dlgStyle.btnHorizontal.setChecked(not self.isVertical)
         if dlgStyle.exec_()==QDialog.Accepted:
             self.view.setZoomFactor(dlgStyle.spbZoom.value())
             self.color = dlgStyle.color
             self.bgColor = dlgStyle.bgColor
-            self.isVertical = dlgStyle.btnVertical.isChecked()
             self.view.reload()
             self.pageIndex = 0
 
     def gotoPage(self, diff = 0):
-        if self.isVertical:
-            # prevent crash if javascript failed to get pageCount
-            if not self.pageCount:
-                self.pageCount = round(self.view.page().contentsSize().height() / self.view.height())
-            pageHeight = self.view.page().contentsSize().height() / self.pageCount
-            self.pageIndex = round(self.view.page().scrollPosition().y() / pageHeight)
-        else:
-            if not self.pageCount:
-                self.pageCount = round(self.view.page().contentsSize().width() / self.view.width())
-            pageWidth = self.view.page().contentsSize().width() / self.pageCount
-            self.pageIndex = round(self.view.page().scrollPosition().x() / pageWidth)
+        # prevent crash if javascript failed to get pageCount
+        if not self.pageCount:
+            self.pageCount = round(self.view.page().contentsSize().height() / self.view.height())
+        pageHeight = self.view.page().contentsSize().height() / self.pageCount
+        self.pageIndex = round(self.view.page().scrollPosition().y() / pageHeight)
 
         # Change page number
         if diff < 0:
@@ -400,7 +307,6 @@ Description: {2}''').format(title, author, description))
         if self.pageIndex < 0:
             if self.docIndex > 0:
                 self.docIndex -= 1
-                self.setButtons()
                 self.view.load(QUrl.fromLocalFile(self.doc[self.docIndex]))
             else:
                 self.pageIndex = 0
@@ -410,18 +316,13 @@ Description: {2}''').format(title, author, description))
             if self.docIndex < len(self.doc) - 1:
                 self.docIndex += 1
                 self.pageIndex = 0
-                self.setButtons()
                 self.view.load(QUrl.fromLocalFile(self.doc[self.docIndex]))
             else:
                 self.pageIndex = self.pageCount - 1
             return
 
-        if self.isVertical:
-            self.view.page().runJavaScript("window.scrollTo({0}, {1});"
-                .format(self.view.page().scrollPosition().x(), pageHeight / self.view.zoomFactor() * self.pageIndex))
-        else:
-            self.view.page().runJavaScript("window.scrollTo({0}, {1});"
-                .format(pageWidth / self.view.zoomFactor() * self.pageIndex, self.view.page().scrollPosition().y()))
+        self.view.page().runJavaScript("window.scrollTo({0}, {1});"
+            .format(self.view.page().scrollPosition().x(), pageHeight / self.view.zoomFactor() * self.pageIndex))
 
     @pyqtSlot()
     def on_action_Search_triggered(self):
@@ -437,8 +338,7 @@ Description: {2}''').format(title, author, description))
         self.posY = self.view.page().scrollPosition().y()
 
         # Show scroll bar so that scrolling works when searching
-        if self.actionPaged.isChecked():
-            self.view.page().runJavaScript("document.body.style.overflow = 'auto';")
+        self.view.page().runJavaScript("document.body.style.overflow = 'auto';")
 
         self.search()
 
